d770037fb225ab347751443fccc4f25c791cf866
HaiHuiWang <wanghh2000@163.com>
2020-02-21 08:06:34 +0800

    test

--- src/util/Hello.scala
+++ src/util/Hello.scala
@@ -4,6 +4,9 @@ object Hello {
   def main(args: Array[String]) {
 
     println("Hello world!");
-
+    var a = List(1, 2, 3, 4, 5);
+    println(a);
+    var b = a.reduce(_+_)
+    println(b);
   }
 }
